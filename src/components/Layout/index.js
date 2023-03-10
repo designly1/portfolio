8b492c8990d72552e4a33b906fca4dbe8c84cde7
Jay Simons <jay@designly.biz>
2023-03-10 10:34:11 -0600

    Bg Image

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -16,13 +16,7 @@ export default function Layout({ pageTitle, children }) {
             <div className="h-screen text-white">
                 <div className="flex fixed top-0 right-0 bottom-0 left-0" style={{ zIndex: -1 }}>
                     <div className="flex shrink-0">
-                        <Image
-                            src={coverImg}
-                            alt="Cover Image"
-                            className="mx-auto min-[1920px]:w-[100vw] opacity-50"
-                            width={1920}
-                            height={1080}
-                        />
+
                     </div>
                 </div>
                 {children}
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,8 @@ import Image from 'next/image';
 import Layout from '@/components/Layout'
 import TypeWriter from '@/components/Home/TypeWriter';
 
+import coverImg from '@/img/cover.jpg';
+
 export default function HomePage() {
   return (
     <Layout>
@@ -11,7 +13,13 @@ export default function HomePage() {
         <div className="w-[300px] bg-bg1">
           sidebar
         </div>
-        <div className="flex flex-col flex-grow">
+        <div className="flex flex-col flex-grow relative">
+          <Image
+            src={coverImg}
+            alt="Cover Image"
+            className="bg-img opacity-50"
+            fill
+          />
           <TypeWriter appendClass="m-auto" />
         </div>
       </div>
