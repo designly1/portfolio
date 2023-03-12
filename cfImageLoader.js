7b3d343ff1ea2207c4acdb6915ef362f4a5b4fc2
Jay Simons <jay@designly.biz>
2023-03-12 18:57:24 -0500

    Add cfImageLoader

---
+++ cfImageLoader.js
@@ -0,0 +1,4 @@
+export default function cfImageLoader({ src, width, quality }) {
+    const params = [`width=${width}`, `quality=${quality || 75}`, 'format=auto']
+    return `https://jay.designly.biz/cdn-cgi/image/${params.join(',')}/${src}`
+}
