6d848d28300c87d85d8f28fa466411842cbf9ded
Jay Simons <jay@designly.biz>
2024-12-18 22:14:55 -0600

    init

--- next.config.js
+++ next.config.js
@@ -1,36 +1,30 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
-const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
 
 const commonConfig = {
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    config.module.rules.push(
-      {
-        test: /\.(md|txt|html|pdf)$/i,
-        loader: "raw-loader",
-      },
-    );
+	webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+		config.module.rules.push({
+			test: /\.(md|txt|html|pdf)$/i,
+			loader: 'raw-loader',
+		});
 
-    // Important: return the modified config
-    return config;
-  },
-}
+		// Important: return the modified config
+		return config;
+	},
+};
 
 module.exports = (phase, { defaultConfig }) => {
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    return {
-      reactStrictMode: true,
-      ...commonConfig
-    }
-  }
+	if (phase === PHASE_DEVELOPMENT_SERVER) {
+		return {
+			reactStrictMode: true,
+			...commonConfig,
+		};
+	}
 
-  // Production config
-  return {
-    swcMinify: true,
-    images: {
-      loader: 'custom',
-      loaderFile: './cfImageLoader.js'
-    },
-    ...commonConfig
-  }
-}
+	// Production config
+	return {
+		swcMinify: true,
+		...commonConfig,
+	};
+};
