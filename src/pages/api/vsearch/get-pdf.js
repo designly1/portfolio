e37782a49fb299dedccab129f06c145362f6c08a
Jay Simons <jay@designly.biz>
2024-12-18 22:28:15 -0600

    init

--- src/pages/api/vsearch/get-pdf.js
+++
@@ -1,8 +0,0 @@
-export const config = {
-    runtime: 'edge',
-}
-
-export default async function handler() {
-    const result = await fetch('https://cdn.designly.biz/pdf/hackers.pdf');
-    return new Response(result);
-}
